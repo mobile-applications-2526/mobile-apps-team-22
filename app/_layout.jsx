33f897256f5a54fcea83d85e321d352a9bccdeb3
SergenUCLL <sergen.schoufs@student.ucll.be>
2025-10-31 14:16:03 +0100

    Revert "deleted old header layout"
    This reverts commit 8ae58a39667aff2a70f0cf313683af545000e8c4.

---
+++ app/_layout.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { Colors } from "../constants/Colors";
+import { StatusBar } from "expo-status-bar";
+
+const RootLayout = () => {
+  // i dont know if i like statusbar white but thats what figma says
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: "Hey there!" }} />
+        <Stack.Screen name="menu" options={{ title: "Menu" }} />
+        <Stack.Screen name="locations" options={{ title: "Locations" }} />
+        <Stack.Screen name="profile" options={{ title: "Profile" }} />
+      </Stack>
+    </>
+  );
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
