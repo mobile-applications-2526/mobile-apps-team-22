940921969c856d3574d0daaeb9353f4f455300d7
XanderDhondtUCLL <xander.dhondt@live.be>
2025-11-18 19:17:21 +0100

    Added pictures to menu items

--- app/(tabs)/menu.jsx
+++ app/(tabs)/menu.jsx
@@ -1,25 +1,29 @@
-import { View, Text, ScrollView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import MenuCard from '../../components/menu/menuCard';
-import { menuItems } from '../../assets/mockData/mockData';
-import tw from 'twrnc';
+import { View, Text, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MenuCard from "../../components/menu/menuCard";
+import { menuItems } from "../../assets/mockData/mockData";
+import tw from "twrnc";
 
 const Menu = () => {
-  const types = [...new Set(menuItems.map(item => item.type))];
+  const types = [...new Set(menuItems.map((item) => item.type))];
 
   return (
     <SafeAreaView style={tw`flex-1 bg-white`}>
       <ScrollView contentContainerStyle={tw`px-5`}>
         <View style={tw`px-5 py-4 border-b border-gray-200 w-full`}>
           <Text style={tw`text-2xl font-bold`}>Menu</Text>
-          <Text style={tw`text-base text-gray-600 mt-1`}>Explore our offerings.</Text>
+          <Text style={tw`text-base text-gray-600 mt-1`}>
+            Explore our offerings.
+          </Text>
         </View>
 
         {types.map((type) => {
           const itemsOfType = menuItems.filter((item) => item.type === type);
           return (
             <View key={type}>
-              <Text style={tw`mt-10 mx-5 font-bold text-xl`}>{`${type}${type.endsWith('s') ? '' : 's'}`}</Text>
+              <Text style={tw`mt-10 mx-5 font-bold text-xl`}>{`${type}${
+                type.endsWith("s") ? "" : "s"
+              }`}</Text>
               <View style={tw`flex-row flex-wrap justify-between`}>
                 {itemsOfType.map((item, idx) => (
                   <View key={item.id ?? idx} style={tw`w-1/2 p-1 my-4 `}>
--- components/menu/menuCard.jsx
+++ components/menu/menuCard.jsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import tw from 'twrnc';
+import React from "react";
+import { View, Text } from "react-native";
+import tw from "twrnc";
+import { Image } from "expo-image";
 
 const CARD_WIDTH = 160;
 const CARD_HEIGHT = 180;
 
 const MenuCard = ({ menuItem }) => {
-    return (
-        <View
-            style={[
-                tw`bg-[#E1503F] rounded-lg overflow-hidden}`,
-                { width: CARD_WIDTH, height: CARD_HEIGHT },
-            ]}
-        >
-            <View style={tw`h-3/5 bg-[#EEDAB5] items-center justify-center rounded-lg`}>
-                <Text>Foto</Text>
-            </View>
+  return (
+    <View
+      style={[
+        tw`bg-[#E1503F] rounded-lg overflow-hidden}`,
+        { width: CARD_WIDTH, height: CARD_HEIGHT },
+      ]}
+    >
+      <View
+        style={tw`h-3/5 bg-[#EEDAB5] items-center justify-center rounded-lg`}
+      >
+        <Image
+          source={menuItem.image}
+          style={tw`w-full h-full rounded-lg`}
+          contentFit="cover"
+        />
+      </View>
 
-            <View style={tw`p-3 flex-1 justify-center items-center`}>
-                <Text style={tw`text-[#F5F2EC] font-semibold text-center`}>{menuItem.name}</Text>
-            </View>
-        </View>
-    );
+      <View style={tw`p-3 flex-1 justify-center items-center`}>
+        <Text style={tw`text-[#F5F2EC] font-semibold text-center`}>
+          {menuItem.name}
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default MenuCard;
